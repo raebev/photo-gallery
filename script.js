3a1f398d3e8724b8c31a750efa1ec6f6739641c7
Rae <34218608+raebev@users.noreply.github.com>
2023-03-24 16:22:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var next = document.getElementById(next);
+var previous = document.getElementById(previous);
+let images = [];
+
+images.push('images/fabric.jpg');
+images.push('images/herbs.jpg');
+images.push('images/swatches.jpg');
+images.push('images/swatches2.jpg');
+
+console.log(images);
